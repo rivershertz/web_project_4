85de7b9632703a9edbd4cc89ed5c3f6bdcd6cd6d
Nahar Shertz <river16@gmail.com>
2022-01-21 00:56:41 +0200

    A

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,17 @@
-initialCardsUpload();
-const formContainer = document.querySelector(".popup");
+const imageFormContainer = document.querySelector(".popup-image");
+const profileFormContainer = document.querySelector(".popup-profile");
 const profileName = document.querySelector(".profile__name");
 const profileAbout = document.querySelector(".profile__subtitle");
 const editBtn = document.querySelector(".profile__edit");
-const saveBtn = document.querySelector(".popup__save");
-const closeBtn = document.querySelector(".popup__close");
+const addBtn = document.querySelector(".profile__add");
+const profileSaveBtn = document.querySelector(".profile__save");
+const imageCreateBtn = document.querySelector(".image__create");
+const profileCloseBtn = document.querySelector(".profile__close");
+const imageCloseBtn = document.querySelector(".image__close");
 const inputName = document.querySelector(".popup__name");
 const inputAbout = document.querySelector(".popup__about");
+const inputTitle = document.querySelector(".popup__title");
+const inputUrl = document.querySelector(".popup__url");
 const initialCards = [
   {
     name: "Yosemite Valley",
@@ -34,12 +39,27 @@ const initialCards = [
   }
 ];
 
-function handlePopupFormToggle() {
-  formContainer.classList.toggle("popup_opened");
+function initialCardsUpload () {
+  initialCards.forEach(function (item) {
+    const cardTemplate = document.querySelector("#card-template").content;
+    const cardElement = cardTemplate.querySelector(".photos__card").cloneNode(true);
+    cardElement.querySelector(".photos__title").textContent = item.name;
+    cardElement.querySelector(".photos__img").src = item.link;
+    document.querySelector(".photos__list").append(cardElement);
+  });
+}
+initialCardsUpload();
+
+function handleProfileFormToggle() {
+  profileFormContainer.classList.toggle("popup_opened");
 }
 
-function handlePopupFormOpen() {
-  handlePopupFormToggle()
+function handleImageAddFormToggle() {
+  imageFormContainer.classList.toggle("popup_opened");
+}
+
+function handleProfileFormOpen() {
+  handleProfileFormToggle()
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
 }
@@ -48,28 +68,25 @@ function handleProfileFormSave(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  handlePopupFormToggle()
+  handleProfileFormToggle()
 }
 
-function initialCardsUpload (card) {
-  console.dir(document.querySelector('#card-template'))
+function handleImageAddFormCreate(evt) {
+  evt.preventDefault();
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.querySelector(".photos__card").cloneNode(true);
-  initialCards.forEach(function (item) {
-    cardElement.querySelector(".photos__title").textContent = item.name;
-    cardElement.querySelector(".photos__img").src = item.link;
-    document.querySelector(".photos__list").append(cardElement);
-  });
-}
-
-function addCardSubmit (title, link) {
-  const cardTemplate = document.querySelector("#card-template").content;
-  const cardElement = cardTemplate.querySelector(".photos__card").cloneNode(true);
-  cardElement.querySelector(".photos__title").textContent = title;
-  cardElement.querySelector(".photos__img").src = link;
+  let imageUrl = document.querySelector(".photos__img").src;
+  const imageTitle = document.querySelector(".photos__title");
+  imageTitle.textContent = inputTitle.value;
+  imageUrl = `" + ${inputUrl.value} + "`;
   document.querySelector(".photos__list").prepend(cardElement);
-};
+  handleImageAddFormToggle()
+}
 
-formContainer.addEventListener("submit", handleProfileFormSave);
-editBtn.addEventListener("click", handlePopupFormOpen);
-closeBtn.addEventListener("click", handlePopupFormToggle);
+profileFormContainer.addEventListener("submit", handleProfileFormSave);
+imageFormContainer .addEventListener("submit", handleImageAddFormCreate);
+editBtn.addEventListener("click", handleProfileFormOpen);
+profileCloseBtn.addEventListener("click", handleProfileFormToggle);
+addBtn.addEventListener("click", handleImageAddFormToggle);
+imageCloseBtn.addEventListener("click", handleImageAddFormToggle);
+
