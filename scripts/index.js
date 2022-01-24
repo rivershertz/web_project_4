57f343d10ed736ee7ed82eca5748e57cc7032b6a
Nahar Shertz <river16@gmail.com>
2022-01-25 01:02:23 +0200

    semifix_sub_1

--- scripts/index.js
+++ scripts/index.js
@@ -13,44 +13,20 @@ const inputName = document.querySelector(".popup__name");
 const inputAbout = document.querySelector(".popup__about");
 const inputUrl = document.querySelector(".popup__input_url");
 const inputTitle = document.querySelector(".popup__input_title");
-const popupImageImg = document.querySelector(".image-popup__img");
+const popupImageImg = imageFormContainer.querySelector(".image-popup__img");
 const popupImageTitle = document.querySelector(".image-popup__title");
 const popupImageClose = document.querySelector(".image-popup__close");
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
-  }
-];
+const cardTemplate = document.querySelector("#card-template").content;
+const photosList = document.querySelector(".photos__list");
 
 function initialCardsUpload () {
   initialCards.forEach(function (item) {
-    const cardTemplate = document.querySelector("#card-template").content;
     const cardElement = cardTemplate.querySelector(".photos__card").cloneNode(true);
-    cardElement.querySelector(".photos__title").textContent = item.name;
-    cardElement.querySelector(".photos__img").src = item.link;
     const likeBtn = cardElement.querySelector(".photos__like");
-    let imageUrl = cardElement.querySelector(".photos__img");
+    const imageUrl = cardElement.querySelector(".photos__img");
+    cardElement.querySelector(".photos__title").textContent = item.name;
+    imageUrl.src = item.link;
+    imageUrl.alt = `picture of ${item.name}`;
     likeBtn.addEventListener("click", function(evt) {
     evt.target.classList.toggle("photos__like_active");
   });
@@ -63,31 +39,23 @@ function initialCardsUpload () {
       const imageTitle = cardElement.querySelector(".photos__title");
       popupImageImg.src = imageUrl.src;
       popupImageTitle.textContent = imageTitle.textContent;
-      imagePopupContainer.classList.toggle("popup_opened");
+      openPopup(imagePopupContainer);
     });
-    document.querySelector(".photos__list").append(cardElement);
+    photosList.append(cardElement);
   });
 }
 initialCardsUpload();
 
-function handleProfileFormOpen() {
-  profileFormContainer.classList.toggle("popup_opened");
+function openPopup(popupElement) {
+  popupElement.classList.add("popup_opened");
 }
 
-function handleImageAddFormToggle() {
-  imageFormContainer.classList.toggle("popup_opened");
-}
-
-function handleProfileFormToggle() {
-  profileFormContainer.classList.toggle("popup_opened");
-}
-
-function handleImagePopupToggle () {
-  imagePopupContainer.classList.toggle("popup_opened");
+function closePopup(popupElement) {
+  popupElement.classList.toggle("popup_opened");
 }
 
 function handleProfileFormOpen() {
-  handleProfileFormToggle()
+  openPopup(profileFormContainer);
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
 }
@@ -96,17 +64,17 @@ function handleProfileFormSave(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  handleProfileFormToggle()
+  closePopup(profileFormContainer);
 }
 
 function handleImageAddFormCreate(evt) {
   evt.preventDefault();
-  const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.querySelector(".photos__card").cloneNode(true);
   const imageTitle = cardElement.querySelector(".photos__title");
-  let imageUrl = cardElement.querySelector(".photos__img");
+  const imageUrl = cardElement.querySelector(".photos__img");
   imageTitle.textContent = inputTitle.value;
   imageUrl.src = inputUrl.value;
+  imageUrl.alt = `picture of ${inputTitle}`;
   const likeBtn = cardElement.querySelector(".photos__like");
   likeBtn.addEventListener("click", function(evt) {
     evt.target.classList.toggle("photos__like_active");
@@ -119,22 +87,18 @@ function handleImageAddFormCreate(evt) {
   imageUrl.addEventListener("click", function() {
     popupImageImg.src = imageUrl.src;
     popupImageTitle.textContent = imageTitle.textContent;
-    handleImagePopupToggle();
+    popupImageImg.alt = `picture of ${inputTitle}`;
+    debugger
+    openPopup(imagePopupContainer);
   });
-  document.querySelector(".photos__list").prepend(cardElement);
-  handleImageAddFormToggle();
+  photosList.prepend(cardElement);
+  closePopup(imageFormContainer);
 }
 
 profileFormContainer.addEventListener("submit", handleProfileFormSave);
 imageFormContainer.addEventListener("submit", handleImageAddFormCreate);
-editBtn.addEventListener("click", handleProfileFormOpen);
-profileCloseBtn.addEventListener("click", handleProfileFormToggle);
-addBtn.addEventListener("click", handleImageAddFormToggle);
-imageCloseBtn.addEventListener("click", handleImageAddFormToggle);
-popupImageClose.addEventListener("click", handleImagePopupToggle);
-
-
-
-
-
-
+editBtn.addEventListener("click", () => openPopup(profileFormContainer));
+profileCloseBtn.addEventListener("click", () => closePopup(profileFormContainer));
+addBtn.addEventListener("click", () => openPopup(imageFormContainer));
+imageCloseBtn.addEventListener("click", () => closePopup(imageFormContainer));
+popupImageClose.addEventListener("click", () => closePopup(imagePopupContainer));
