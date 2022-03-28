76492e879e5c17922417d2722f4117c21f27b9f5
Nahar Shertz <river16@gmail.com>
2022-03-28 22:35:03 +0300

    add webpack complete

---
+++ src/components/utils.js
@@ -0,0 +1,31 @@
+export { closePopup, closeByEscape, openPopup, addRemoteClickListeners };
+
+
+
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+  document.addEventListener("keydown", closeByEscape);
+}
+
+function closePopup(popup) {
+  document.removeEventListener("keydown", closeByEscape);
+  popup.classList.remove("popup_opened");
+  
+}
+
+const closeByEscape = (evt) => {
+  if (evt.code === "Escape") {
+    const openedPopup = document.querySelector(".popup_opened")
+    closePopup(openedPopup);
+  }
+};
+
+const addRemoteClickListeners = (popupList) => {
+  popupList.forEach((popup) => {
+    popup.addEventListener("mousedown", (evt) => {
+      if (evt.target.matches(".popup")) {
+        closePopup(popup);
+      }
+    });
+  });
+};
