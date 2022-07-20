374432a13afab02000221ae29ce9244c6a731bab
Nahar Shertz <river16@gmail.com>
2022-07-20 20:33:08 +0300

    Updates

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-const presets = [
-    ['@babel/preset-env', {
-      targets: "defaults, not IE 11, not dead",
-      useBuiltIns: "entry",
-      corejs: '^3',
-    }]
-  ];
-  
-  module.exports = { presets };
